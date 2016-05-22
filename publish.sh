c66daf08524bc179835de5b0227db688716d1053
allan <allanliu250@gmail.com>
2016-05-21 22:24:30 -0700

    auto publish

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git add .
+git commit -am'auto publish'
+git push origin master
+echo '\n'
+echo "------------"
+echo '\n'
+echo "site should be published"
+echo '\n'
+echo "------------"
+echo '\n'
